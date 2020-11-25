98c5e319875ce1ea17dcc913c974dbca802f6816
Trent <tyranosevich@gmail.com>
2020-11-24 18:11:47 -0800

    stuff

--- yelp_table_creation.sql
+++ yelp_table_creation.sql
@@ -21,3 +21,76 @@ business_type_name varchar(10) NOT NULL,
 PRIMARY KEY (business_type_id)
 );
 
+
+CREATE TABLE business_attributes (
+business_id char(22) NOT NULL,
+bikeParking boolean,
+businessAcceptsBitcoin boolean,
+businessAcceptsCreditCards boolean,
+garage_parking boolean,
+street_parking boolean,
+dogsAllowed boolean,
+rstaurantsPriceRange2 smallint,
+wheelchairAccessible boolean,
+valet_parking boolean,
+paring_lot boolean,
+PRIMARY KEY (business_id),
+FOREIGN KEY(business_id) REFERENCES business (business_id) ON DELETE CASCADE
+);
+
+CREATE TABLE business_category ( 
+business_id char(22) NOT NULL,
+category_id int NOT NULL,
+PRIMARY KEY (business_id),
+FOREIGN KEY (business_id) REFERENCES business (business_id) ON DELETE CASCADE,
+FOREIGN KEY (category_id) REFERENCES category_ref (category_id) ON DELETE CASCADE
+);
+
+CREATE TABLE category_ref (
+category_id int NOT NULL,
+category_name varchar(10) NOT NULL,
+PRIMARY KEY (category_id)
+);
+
+CREATE TABLE review (
+business_id char(22) NOT NULL,
+review_id varchar(22) NOT NULL,
+user_id char(22) NOT NULL,
+type char(8) NOT NULL,
+cool smallint,
+date timestamp,
+funny smallint,
+stars smallint,
+useful smallint,
+text text,
+PRIMARY KEY (review_id),
+FOREIGN KEY (business_id) REFERENCES business (business_id) ON DELETE CASCADE,
+FOREIGN KEY (user_id) REFERENCES users (user_id) -- To delete here or not. For analysis, no delete. IRL there could be reasons to have this delete cascade.
+);
+
+CREATE TABLE users (
+user_id char(22) NOT NULL,
+name varchar(20) NOT NULL,
+type char(4) NOT NULL,
+yelping_since timestamp NOT NULL,
+PRIMARY KEY (user_id)
+);
+
+CREATE TABLE relationships (
+user1_id char(22) NOT NULL,
+user2_id char(22) NOT NULL,
+FOREIGN KEY (user1_id) REFERENCES users (user_id) ON DELETE CASCADE,
+FOREIGN KEY (user2_id) REFERENCES users (user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE tip (
+business_id char(22) NOT NULL,
+user_id char(22) NOT NULL,
+review_date timestamp NOT NULL,
+likes smallint NOT NULL,
+review_text varchar(200) NOT NULL,
+type varchar(10),
+PRIMARY KEY (business_id),
+FOREIGN KEY (business_id) REFERENCES business (business_id) ON DELETE CASCADE,
+FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+);
